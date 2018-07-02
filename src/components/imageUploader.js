e633b1ddbe77c85128f05d26073cf1b862a2170e
Alexander Templeton <blakmatr@gmail.com>
2018-07-02 01:20:51 -0500

    working on photo uploading:

--- src/components/imageUploader.js
+++ src/components/imageUploader.js
@@ -4,6 +4,19 @@ import React, {
 
 import ImageUploader from 'react-images-upload';
 
+Amplify.configure(
+    Auth: {
+        identityPoolId: 'us-east-1:01395ccc-0844-4002-9917-be1b9ad98329', // REQUIRED - Amazon Cognito Identity Pool ID
+    		region: 'us-east-1', 																							// REQUIRED - Amazon Cognito Region
+        userPoolId: 'us-east-1_hb1vHwFWL', 																// OPTIONAL - Amazon Cognito User Pool ID
+        userPoolWebClientId: '4264r3osuh6dg5giimlau0mtcb', 								// OPTIONAL - Amazon Cognito Web Client ID
+    },
+    Storage: {
+        bucket: 'rotate-aircraft-image-uploads', 					// REQUIRED -  Amazon S3 bucket
+        region: 'us-east-1',	// OPTIONAL -  Amazon service region
+    }
+);
+
 export default class AircraftImageUploader extends Component {
     constructor(props) {
         super(props);
